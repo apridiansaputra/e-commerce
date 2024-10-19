2cd955981d2efcd1bddc85e7ff5f43a28ca8da83
Apridian Saputra <74088434+apridiansaputra@users.noreply.github.com>
2024-10-19 21:05:59 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const images = document.querySelector(".document-images img")
+    const navigationWrapper = document.querySelector(".navigation")
+    let currentIndex = 0
+})
+
